062d416f321a9e2b08f79020856ba9008b36207b
Arseniy <arseniykudashkin@gmail.com>
2024-05-05 13:08:36 +0600

    Create ms.sh

---
+++ ms.sh
@@ -0,0 +1,24 @@
+usagemsg="usage: ms <url> \"<trackname>\""
+url=$1
+name=$2
+
+function getTrack ()
+{
+	yt-dlp -x --audio-format mp3 --audio-quality 0 -P ~/music \
+		--no-keep-fragments -o "$name" $url
+}
+
+main ()
+{
+	if [[ -n "$url" && -n "$name" ]]; then
+		getTrack
+	else
+		read -p "url: " url
+		read -p "track: " name
+		[[ -n "$url" && -n "$name" ]] && getTrack
+	fi
+	
+	return 0
+}
+
+main "$@"
